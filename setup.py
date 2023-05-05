c9f29b5590e3ad4543ff10748cd7f3e8084aa7f3
Luke Pollen <luke.pollen@cytiva.com>
2023-05-05 16:56:55 +0700

    0.1.6 - Successful implementation of table creation logic for secondary project, web crawling, with successful data writes to the cloud.
    Beaver on.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='data-beaver',
-    version='0.1.0',
-    packages=find_packages(),
+    name='metabeaver',
+    version='0.1.6', # Major, minor, patch
+    packages=find_packages(exclude=['Testing', '*.xlsx', '*.xls']),
     install_requires=[
         'numpy',
         'pandas',
@@ -15,3 +15,20 @@ setup(
     author_email='luke@pollenanalytics.com',
     url='https://github.com/rainbowpusheenthe3rd/dataBeaver',
 )
+
+
+##### ERRATA #####
+
+#QHFG25#DQLPMeEy
+
+#python setup.py sdist bdist_wheel
+
+
+#pip show twine
+#dir /s twine.exe
+
+
+#twine upload dist/*
+#twine upload *
+
+##### ERRATA #####
