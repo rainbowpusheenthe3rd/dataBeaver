d8a086c0b338e3d3bb4f1ceff741db16909e99ab
Luke Pollen <luke.pollen@cytiva.com>
2023-09-03 00:29:34 +0700

    Defined a conditional print through the use of operating system module (os). Called via metabeaver.Formatting.printControl.conditional_print
    Beaver on.

--- metabeaver/Formatting/printControl.py
+++ metabeaver/Formatting/printControl.py
@@ -1,33 +1,15 @@
-# Import the printControl manager
-from metabeaver.Formatting.printControl import enable_print
+import os
 
-class PrintControl:
-    def __init__(self, enabled=True):
-        self.enabled = enabled
-        self.original_enabled = enabled
-
-    def __enter__(self):
-        self.original_enabled = self.enabled
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.enabled = self.original_enabled
 
 def conditional_print(message):
-    # Use the global print_control
-    global print_control
-    if print_control and print_control.enabled:
+
+    # Try to get a boolean BEAVER_PRINTING operating system variable
+    try:
+        beaverPrint = os.environ.get("BEAVER_PRINTING")
+    # If we can not find the BEAVER_PRINTING variable, assume printing is not wanted
+    except Exception as e:
+        beaverPrint = False
+
+    # Print if we enabled BEAVER_PRINTING by setting it to True
+    if beaverPrint == 'True':
         print(message)
-
-# Usage example:
-def yesPrint():
-    # Your code using conditional_print here
-    conditional_print('Inside yesPrint!')
-    # Rest of your code
-
-def noPrint():
-    # Your code using conditional_print here
-    conditional_print('Inside noPrint!')
-    # Rest of your code
-
-
--- metabeaver/Formatting/printControlTest.py
+++ metabeaver/Formatting/printControlTest.py
@@ -1,22 +1,40 @@
-from metabeaver.Formatting.printControl import PrintControl, conditional_print as cprint
+from metabeaver.Formatting.printControl import conditional_print as cprint
+
+
+import os
 
 
 # Usage example:
 def yesPrint():
-    # Your code using conditional_print here
-    global print_control
+
+    # Update BEAVER_PRINTING to be True. str type because we are creating an env var.
+    os.environ["BEAVER_PRINTING"] = 'True'
+
     cprint('Inside yesPrint!')
     # Rest of your code
 
+
 def noPrint():
-    # Your code using conditional_print here
+
+    # Update BEAVER_PRINTING to be False. str type because we are creating an env var.
+    os.environ["BEAVER_PRINTING"] = 'False'
+
     cprint('Inside noPrint!')
     # Rest of your code
 
 
-# Initialize the global print_control to enabled=False to disable printing by default
-print_control = PrintControl(enabled=True)  # Change to False if you want to disable printing globally
-yesPrint()
+def someFunction(argOne, argTwo):
 
-print_control = PrintControl(enabled=False)  # Change to False if you want to disable printing globally
+    cprint('Roses are red, Violets are blue, Beaver teeth are orange and they chew through wood too!')
+
+# Set the environment variable to str 'True' and print
+os.environ["BEAVER_PRINTING"] = 'True'
+someFunction('', '')
+
+# Set the environment variable to str 'False' and do not print. Call function that does not set value internally.
+os.environ["BEAVER_PRINTING"] = 'False'
+someFunction('', '')
+
+# Call functions which modify printing within themselves
+yesPrint()
 noPrint()
